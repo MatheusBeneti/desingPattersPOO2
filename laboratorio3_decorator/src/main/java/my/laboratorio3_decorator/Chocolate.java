84adbaf903758f46dda3f77565c0e443fb3b97a8
Beneti <matheusbeneti2@gmail.com>
2024-02-03 19:14:01 -0300

    done!

---
+++ laboratorio3_decorator/src/main/java/my/laboratorio3_decorator/Chocolate.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my.laboratorio3_decorator;
+
+/**
+ *
+ * @author mathe
+ */
+public class Chocolate extends decoratorCoffe{
+
+    public Chocolate(ICoffee coffee) {
+        super(coffee);
+    }
+
+
+    public float calculeCost() {
+        return super.calculeCost();
+    }
+    
+
+    public String getDescription() {
+        return super.getDescription() + " Chocolate ";
+    }
+    
+}
---
+++ laboratorio3_decorator/src/main/java/my/laboratorio3_decorator/Laboratorio3_decorator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package my.laboratorio3_decorator;
+
+/**
+ *
+ * @author mathe
+ */
+public class Laboratorio3_decorator {
+
+    public static void main(String[] args) {
+        
+        ICoffee express = new Milk(new Chocolate(new Espresso()));
+        System.out.println(express.calculeCost());
+        System.out.println(express.getDescription());
+        
+        ICoffee decaf = new Chocolate(new Milk(new Decaf()));
+        System.out.println(decaf.calculeCost());
+        System.out.println(decaf.getDescription());
+
+    }
+}
